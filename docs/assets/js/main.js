e7f3631d4d76b7b0126887d36651299f46ee3997
Scillman <Scillman@users.noreply.github.com>
2024-02-21 19:14:32 +0100

    sidenav and version-dropdown updated

---
+++ docs/assets/js/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidenav").style.width = "350px";
+    //document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    //document.body.style.backgroundColor = "rgba(30,30,30,1.0)";
+}
